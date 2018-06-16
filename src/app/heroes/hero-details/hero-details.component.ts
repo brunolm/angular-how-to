9c0e7dcba8d6f3c8d6102eb1749f8ed8bed99da7
Bruno Leonardo Michels <brunolm7@gmail.com>
2018-06-16 04:47:52 -0300

    Add ngx-take-until-destroy to unsubscribe observables

--- src/app/heroes/hero-details/hero-details.component.ts
+++ src/app/heroes/hero-details/hero-details.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Hero } from '../shared/hero.model';
-
+import { TakeUntilDestroy, untilDestroyed } from 'ngx-take-until-destroy';
 import { map } from 'rxjs/operators';
+
+import { Hero } from '../shared/hero.model';
 
 @Component({
   selector: 'app-hero-details',
   templateUrl: './hero-details.component.html',
   styleUrls: ['./hero-details.component.scss'],
 })
-export class HeroDetailsComponent implements OnInit {
+@TakeUntilDestroy()
+export class HeroDetailsComponent implements OnInit, OnDestroy {
   heroes = [
     new Hero({ id: 1, name: 'Saitama' }),
     new Hero({ id: 2, name: 'Goku' }),
@@ -21,9 +23,15 @@ export class HeroDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.paramMap.pipe(map((params) => +params.get('id'))).subscribe((id) => {
-      this.activeHero = this.heroes.find((hero) => hero.id === id);
-    });
+    this.route.paramMap
+      .pipe(map((params) => +params.get('id')))
+      .pipe(untilDestroyed(this))
+      .subscribe((id) => {
+        this.activeHero = this.heroes.find((hero) => hero.id === id);
+      });
+  }
+
+  ngOnDestroy() {
   }
 
   byId(_, hero: Hero) {
