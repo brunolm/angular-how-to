a263625557836a603b436ada4253cab5315550de
Bruno Leonardo Michels <brunolm7@gmail.com>
2018-06-16 02:34:53 -0300

    Add form with validation and messages

---
+++ src/app/heroes/hero-edit/hero-edit.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeroEditComponent } from './hero-edit.component';
+
+describe('HeroEditComponent', () => {
+  let component: HeroEditComponent;
+  let fixture: ComponentFixture<HeroEditComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeroEditComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/heroes/heroes.router.ts
+++ src/app/heroes/heroes.router.ts
@@ -1,11 +1,17 @@
 import { RouterModule, Routes } from '@angular/router';
 
+import { HeroEditComponent } from './hero-edit/hero-edit.component';
 import { HeroesComponent } from './heroes.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HeroesComponent,
+  },
+
+  {
+    path: 'new',
+    component: HeroEditComponent,
   },
 ];
 
