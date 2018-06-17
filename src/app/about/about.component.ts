8f7dbfa17126a5a4d358544817856ec9574f2031
Bruno Leonardo Michels <brunolm7@gmail.com>
2018-06-17 18:58:22 -0300

    Add example of action call and values

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { NgxReduxStatePropsService } from 'ngx-redux-state-props';
+
+import { AboutActions } from './services/about.actions';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  constructor(private actions: AboutActions, private redux: NgxReduxStatePropsService) {}
+
+  get state() {
+    return this.redux.appState;
+  }
+
+  testRedux() {
+    this.actions.getApiValue();
+  }
+}
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AboutComponent } from './about.component';
 import { RoutingModule } from './about.router';
+import { AboutActions } from './services/about.actions';
 
 @NgModule({
   imports: [RoutingModule, CommonModule],
   declarations: [AboutComponent],
+  providers: [AboutActions],
 })
 export class AboutModule {}
